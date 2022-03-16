d26363f989462c5bbf935fdd9985951546e10bfe
Guillem Perez Delgado <guillem.perez.delgado@gmail.com>
2022-03-16 20:03:17 +0100

    Improve readability of Camera class by using constants

--- Camera.cpp
+++ Camera.cpp
@@ -4,10 +4,11 @@
 
 #include <glm/ext/matrix_transform.hpp>
 #include <glm/ext/matrix_clip_space.hpp>
+#include <glm/ext/scalar_constants.hpp>
 
 #include <glm/matrix.hpp>
 
-#include <GLFW/glfw3.h>
+#include <GLFW/glfw3.h> // TODO: Remove and use Window.h instead
 
 Camera::Camera()
     : m_position(0.0f,  0.0f,  0.0f)
@@ -15,9 +16,9 @@ Camera::Camera()
     , m_up      (0.0f,  1.0f,  0.0f)
     , m_forward (0.0f,  0.0f, -1.0f)
     , m_azimuth(0.0f)
-    , m_zenith(3.1415f / 2.0f)
+    , m_zenith(glm::half_pi<float>())
     , m_isRotating(false)
-    , m_verticalFov(3.1415f / 4.0f)
+    , m_verticalFov(glm::half_pi<float>())
 {
 }
 
@@ -25,8 +26,8 @@ void Camera::OnRender()
 {
     if (ImGui::Begin("Camera"))
     {
-        ImGui::SliderFloat("Azimuth", &m_azimuth, 0.0f, 2.0f * 3.14f);
-        ImGui::SliderFloat("Vertical Fov", &m_verticalFov, 3.1415f / 8.0f, 3.1415f / 2.0f);
+        ImGui::SliderFloat("Azimuth", &m_azimuth, 0.0f, glm::two_pi<float>());
+        ImGui::SliderFloat("Vertical Fov", &m_verticalFov, glm::quarter_pi<float>(), glm::two_thirds<float>() * glm::pi<float>());
     }
     ImGui::End();
 
@@ -50,10 +51,11 @@ void Camera::OnMouseMovement(glm::vec2 movement)
 
     // m_isRotating
     m_azimuth += 0.01f * movement.x;
-    m_azimuth = glm::mod(m_azimuth, 2.0f * 3.1415f);
+    m_azimuth = glm::mod(m_azimuth, glm::two_pi<float>());
 
+    constexpr float safetyMargin = 0.25f;
     m_zenith += 0.01f * movement.y;
-    m_zenith = glm::clamp(m_zenith, 0.25f, 3.1415f - 0.25f);
+    m_zenith = glm::clamp(m_zenith, safetyMargin, glm::two_pi<float>() - safetyMargin);
 }
 
 void Camera::OnMouseClick(int button, int action, int mods)
@@ -78,5 +80,7 @@ glm::mat4 Camera::GetViewMatrix() const
 
 glm::mat4 Camera::GetProjectionMatrix() const
 {
+    constexpr float zNear = 0.01f;
+    constexpr float zFar = 100.0f;
     return glm::perspective(m_verticalFov, m_aspectRatio, 0.01f, 100.0f);
 }
